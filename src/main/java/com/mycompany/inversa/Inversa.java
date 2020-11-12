f988d2eaf380f3205772d043329e355748862047
Jorge Licea <Jorge Licea@192.168.1.70>
2020-11-12 14:56:54 -0600

    Metodo pedir y clase main

---
+++ src/main/java/com/mycompany/inversa/Inversa.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.inversa;
+     import java.util.Scanner;
+
+/**
+ *
+ * @author Jorge Licea
+ */
+public class Inversa 
+{
+//@author Jorge Licea
+//@author Luis Hernandez
+
+    public static void main(String [] args)
+    {
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Numero de filas ");
+        int fil=leer.nextInt();
+        System.out.println("Numero de columnas ");
+        int col=leer.nextInt();
+        
+//crear  objeto Matriz
+        Proceso mat=new Proceso(fil,col);
+        
+        mat.Pedir();
+      //  mat.Licea();
+        
+    }
+}    
+
---
+++ src/main/java/com/mycompany/inversa/Proceso.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.inversa;
+
+/**
+ *
+ * @author Jorge Licea
+ */
+public class Proceso 
+{
+    int fil;
+    int col;
+    public Proceso(int fil,int col)
+    {
+        this.fil=fil;
+        this.col=col;
+    }
+    
+    public void Pedir()
+    {
+        System.out.println("hola");
+    }
+}
