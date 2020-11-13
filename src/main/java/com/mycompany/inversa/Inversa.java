19ccc1338d876ae1c27e728cc37d4dd886eb4245
Oscar420957 <74226628+Oscar420957@users.noreply.github.com>
2020-11-13 12:32:26 -0600

    Update Inversa.java

--- src/main/java/com/mycompany/inversa/Inversa.java
+++ src/main/java/com/mycompany/inversa/Inversa.java
@@ -15,9 +15,16 @@ public class Inversa
 //@author Jorge Licea
 //@author Luis Hernandez
 //@author Oscar Morales
+     
+     public static void Autores()
+    {
+        System.out.println("");
+        System.out.println("Jorge Lice "  + "\nLuis Hernande" + "\nOscar Morales");
+    }
 
     public static void main(String [] args)
     {
+         Autores();
         Scanner leer=new Scanner(System.in);
         System.out.println("Numero de filas ");
         int fil=leer.nextInt();
